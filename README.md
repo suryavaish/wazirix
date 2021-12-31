36e21c08c9efb1cf6f1ac4166d451ad770f19b24
suryavaish <96910270+suryavaish@users.noreply.github.com>
2021-12-31 16:21:38 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Wazirx-API
---
+++ app.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+//get route
+app.get('/', async function (req, res) {
+  const response = await fetch('https://api.wazirx.com/api/v2/tickers');
+  const data = await response.json();
+  const keys = Object.keys(data);
+  res.render('body', { data: data, keys: keys });
+});
+//Post route- here nothing to post
+app.post('/', function (req, res) {});
+//Accessing port
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 4000;
+}
+app.listen(port, function () {
+  console.log('Server started Successfully');
+});
